83d62d3ee448044d05bb48ca782abc34938ffc10
eru123 <yeoligoakino@gmail.com>
2021-07-02 13:16:16 +0800

    added: user read

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -2,7 +2,129 @@ const _ = require('lodash');
 const Joi = require('joi');
 const bcrypt = require('bcrypt')
 module.exports = function (fastify) {
-
+  const createSchema = {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['user', 'pass', 'name'],
+        properties: {
+          user: { type: 'string' },
+          name: { type: 'string' },
+          pass: { type: 'string' }
+        }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "success" },
+            message: { type: 'string' },
+            data: {
+              type: 'object',
+              properties: {
+                _id: { type: 'string' },
+                user: { type: 'string' },
+                name: { type: 'string' }
+              }
+            },
+            token: { type: 'string' }
+          },
+        },
+        400: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string' },
+          }
+        },
+        500: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string' },
+          }
+        }
+      }
+    }
+  }
+  const loginSchema = {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['user', 'pass'],
+        properties: {
+          user: { type: 'string' },
+          pass: { type: 'string' }
+        }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "success" },
+            message: { type: 'string' },
+            token: { type: 'string' }
+          },
+        },
+        404: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string' },
+          }
+        },
+        400: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string' },
+          }
+        },
+        500: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string' },
+          }
+        }
+      }
+    }
+  }
+  const readOneSchema = {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "success" },
+            message: { type: 'string', default: "User found" },
+            data: {
+              type: 'object',
+              properties: {
+                _id: { type: 'string' },
+                user: { type: 'string' },
+                name: { type: 'string' }
+              }
+            },
+          }
+        },
+        404: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string', default: "User not found" },
+          }
+        },
+        500: {
+          type: 'object',
+          properties: {
+            type: { type: 'string', default: "error" },
+            message: { type: 'string' },
+          }
+        }
+      }
+    }
+  }
   function validateUser(user) {
     const schema = Joi.object({
       name: Joi.string().pattern(new RegExp('^[a-zA-Z ]{2,50}$')).min(2).max(50).required(),
@@ -11,7 +133,6 @@ module.exports = function (fastify) {
     })
     return schema.validate(user)
   }
-
   function validateCred(cred) {
     const schema = Joi.object({
       user: Joi.string().alphanum().min(3).max(255).required(),
@@ -19,62 +140,65 @@ module.exports = function (fastify) {
     })
     return schema.validate(cred)
   }
-
+  const newJWTToken = (payload) => {
+    return fastify.jwt.sign(_.pick(payload, ['_id', 'name', 'user']))
+  }
   const create = async (req, res) => {
-
-    const userdata = _.pick(req.body, ['user', "name", "pass"])
-
-    const { error } = validateUser(userdata)
-
+    const { error } = validateUser(req.body)
     if (error) return res.code(400).send({ type: 'error', message: error.details[0].message })
 
-    const duplicate = await fastify.mongoose.Users.findOne({ user: userdata.user })
+    const duplicate = await fastify.mongoose.Users.findOne({ user: req.body.user })
     if ("duplicate:", Boolean(duplicate)) return res.code(400).send({ type: 'error', message: "Username already exists." })
 
-
-    const user = new fastify.mongoose.Users(userdata)
+    const user = new fastify.mongoose.Users(req.body)
     const salt = await bcrypt.genSalt(10)
-    const hashed = await bcrypt.hash(userdata.pass, salt)
+    const hashed = await bcrypt.hash(req.body.pass, salt)
     user.pass = hashed
 
     user.save()
-      .then(() => res.send({
-        type: 'success',
-        message: "User successfully created.",
-        data: _.pick(user, ['_id', "name", "user"])
-      }))
+      .then((u) => {
+        console.log(u)
+        res.send({
+          type: 'success',
+          message: "User successfully created.",
+          data: u,
+          token: newJWTToken(u)
+        })
+      })
       .catch(() => res.code(500).send({
         type: 'error',
         message: "Failed to register. Please contact your administrator to fix this error"
       }))
 
-
   }
-
   const login = async (req, res) => {
     const { error } = validateCred(req.body)
     if (error) return res.code(400).send({ type: 'error', message: error.details[0].message })
 
-    let user = await fastify.mongoose.Users.findOne({ user: req.body.user })
+    const user = await fastify.mongoose.Users.findOne({ user: req.body.user })
     if (!user) return res.code(400).send({ type: 'error', message: 'Invalid username or password' })
     const pass = await bcrypt.compare(req.body.pass, user.pass)
     if (!pass) return res.code(400).send({ type: 'error', message: 'Invalid username or password' })
 
-
-    const token = fastify.jwt.sign({ ..._.pick(user, ['_id', 'name', 'user']), admin: false })
-
     res.send({
       status: "success",
       message: "You are now logged in.",
-      token
+      token: newJWTToken(user)
     })
   }
-
-  const authenticate = { preValidation: [fastify.authenticate] }
-
+  const authenticate = () => ({ preValidation: [fastify.authenticate] })
+  const readOneByUser = async (req, res) => {
+    const user = await fastify.mongoose.Users.findOne({ user: req.params.user })
+    if (!user) return res.code(404).send({})
+    res.send({ data: user })
+  }
   return {
-    authenticate,
     create,
-    login
+    createSchema,
+    loginSchema,
+    readOneSchema,
+    login,
+    authenticate,
+    readOneByUser,
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 module.exports = async function (fastify, options, done) {
-	const { create, login } = fastify.handler.users
+	const { create, createSchema, login, loginSchema, readOneSchema, authenticate, readOneByUser } = fastify.handler.users
 
-	fastify.post("/api/users/register", create)
-	fastify.post("/api/auth/login", login)
+	fastify.post("/api/users/register", createSchema, create)
+	fastify.post("/api/auth/login", loginSchema, login)
+	fastify.get("/api/user/@:user", { ...authenticate(), ...readOneSchema }, readOneByUser)
+
 	done();
 }
