f9a77d3723f5f6dc5bdc13d64d57aea5b0ebdc91
eru123 <yeoligoakino@gmail.com>
2021-08-07 11:41:02 +0800

    remove: fetch userdata

--- aplos/root/src/pages/Home.vue
+++ aplos/root/src/pages/Home.vue
@@ -7,11 +7,3 @@
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium nesciunt iure suscipit cupiditate magni voluptatibus quod ab quisquam impedit adipisci esse laboriosam, error vel cumque est earum officia quae. Eius repellat eum, nesciunt magni, eos quia reiciendis, a praesentium hic ab exercitationem deserunt minima repudiandae consequuntur vero error. Totam at, culpa, dolorem ab distinctio porro animi, voluptate quo id molestias inventore ratione? Accusantium esse cum, aperiam animi maiores excepturi enim. Laboriosam placeat voluptas tempore facilis rem veritatis sequi quae possimus.</p>
   </wf1-main>
 </template>
-<script>
-export default {
-  async created(){
-    const me = await this.$tuos.auth.userMe()
-    console.log(me)
-  }
-}
-</script>
