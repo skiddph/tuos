16fbd16ec6a34b4ecab5d422e119d184f8e449c9
eru123 <yeoligoakino@gmail.com>
2021-08-07 11:36:39 +0800

    added: userdata skeleton loader

---
+++ aplos/root/src/components/UserdataSkeletonLoader.vue
@@ -0,0 +1,27 @@
+<template>
+    <skeleton-loader v-if="userdataState == 'fetching'" :extend-class="extendClass"/>
+    <div v-if="userdataState == 'error'" :class="`${extendClass} fetch-userdata-btn`" @click="fetchUserData">
+        <div class="btn">
+            <i class="fa fa-refresh"></i>
+            <span>Failed to get userdata!</span>
+            <span>Click to fetch userdata again.</span>
+        </div>
+    </div>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+    name:"UserdataSkeletonLoader",
+    props: {
+        extendClass: {
+            extendClass: {
+                type: String,
+                default: ""
+            }
+        }
+    },
+    computed: {
+        ...mapState(['userdataState'])
+    }
+}
+</script>
