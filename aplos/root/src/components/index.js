cd6c9df23bd295caf4bb802bd486e0b4f805eb3a
eru123 <yeoligoakino@gmail.com>
2021-08-07 11:40:11 +0800

    added: skeleton loaders

--- aplos/root/src/components/index.js
+++ aplos/root/src/components/index.js
@@ -1,6 +1,10 @@
 import ConfirmDialog from './ConfirmDialog.vue'
+import SkeletonLoader from './SkeletonLoader.vue'
+import UserdataSkeletonLoader from './UserdataSkeletonLoader.vue'
 export default {
     install: (app) => {
         app.component('confirm-dialog',ConfirmDialog)
+        app.component('skeleton-loader',SkeletonLoader)
+        app.component('userdata-skeleton-loader',UserdataSkeletonLoader)
     }    
 }
