052b564b41e6d19ad054f0c4c9785229f4df2e67
eru123 <yeoligoakino@gmail.com>
2021-08-13 22:37:20 +0800

    refactor

--- tera/auth/ModelUsers.js
+++ tera/auth/ModelUsers.js
@@ -2,23 +2,20 @@ const mongoose = require('mongoose')
 const paginate = require('mongoose-paginate-v2');
 
 
-module.exports = (options) => {
-	const c = options?.config?.plugins?.auth?.users || {}
-	const UsersSchema = new mongoose.Schema({
-		name: { type: String, required: true, },
-		user: { type: String, required: true, unique: true },
-		pass: { type: String, required: true, },
-		created_at: { type: Number, required: true },
-		phone: { type: String, required: false },
-		countryCode: { type: String, required: false },
-		lon: { type: Number, required: false },
-		lat: { type: Number, required: false },
-		phoneVerified: { type: Boolean, required: false },
-		email: { type: String, required: false },
-		emailVerified: { type: Boolean, required: false },
-		defaultPhoto: { type: String, required: false }
-	}, { timestamp: true })
+const UsersSchema = new mongoose.Schema({
+	name: { type: String, required: true, },
+	user: { type: String, required: true, unique: true },
+	pass: { type: String, required: true, },
+	created_at: { type: Number, required: true },
+	phone: { type: String, required: false },
+	countryCode: { type: String, required: false },
+	lon: { type: Number, required: false },
+	lat: { type: Number, required: false },
+	phoneVerified: { type: Boolean, required: false },
+	email: { type: String, required: false },
+	emailVerified: { type: Boolean, required: false },
+	defaultPhoto: { type: String, required: false }
+}, { timestamp: true })
 
-	UsersSchema.plugin(paginate);
-	return mongoose.model(c.model || "Users", UsersSchema)
-}
+UsersSchema.plugin(paginate);
+module.exports =  mongoose.model( "Users", UsersSchema)
