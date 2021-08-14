8060cf68eac70327be82f5ec42596034583040cf
eru123 <yeoligoakino@gmail.com>
2021-08-14 23:38:58 +0800

    update: decrease external api req limit

--- tera/rate-limit/index.js
+++ tera/rate-limit/index.js
@@ -2,7 +2,7 @@ const fp = require("fastify-plugin");
 
 module.exports = fp(async function (fastify, options) {
 	fastify.register(require('fastify-rate-limit'), {
-	  max: 200, 
+	  max: 100, 
 	  timeWindow: 60000,
 	  cache: 10000, 
 	})
