af1fca83ae2640923c009b8212038d41bafe1dae
eru123 <yeoligoakino@gmail.com>
2021-08-18 09:14:54 +0800

    added: tokens db model

---
+++ tera/auth/model.tokens.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const paginate = require('mongoose-paginate-v2');
+
+const TokensSchema = new mongoose.Schema({
+	token: { type: String, required: true, },
+	ip: { type: String, required: true, unique: true },
+	created_at: { type: Number, required: true },
+}, { timestamp: true })
+
+TokensSchema.plugin(paginate);
+module.exports =  mongoose.model( "Tokens", TokensSchema)
