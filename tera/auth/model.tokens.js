eaac07cc0edd614ae09bba1efea7ae1c01633cee
eru123 <yeoligoakino@gmail.com>
2021-08-18 17:17:42 +0800

    added: unique property to token field

--- tera/auth/model.tokens.js
+++ tera/auth/model.tokens.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const paginate = require('mongoose-paginate-v2');
 
 const TokensSchema = new mongoose.Schema({
-	token: { type: String, required: true},
+	token: { type: String, required: true, unique: true },
 	user_id: { type: String, required: true},
 	ip: { type: String, required: true},
     device: { type: String, required: true },
