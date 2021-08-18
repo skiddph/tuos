19c8419b437f7542aba974301fd40eea9bd026bf
eru123 <yeoligoakino@gmail.com>
2021-08-18 23:37:58 +0800

    rewrite: jwt authenticate handler

---
+++ tera/jwt/handler.jwt.js
@@ -0,0 +1,25 @@
+const JwtHandler = (app) => {
+    const { Tokens } = app.mongoose.models
+    
+    const getToken = (req) => req.headers[ 'authorization' ].split(' ')[ 1 ]
+    
+    const authenticate = async (req, res, next) => {
+        const token = getToken(req)
+        const user_id = app.jwt.decode(token)._id
+        
+        const record = await Tokens.findOne({ user_id, token })
+        if (!record) return res.status(401).send({ type: 'error' ,message: 'Unauthorized' })
+        
+        app.jwt.verify(token, null, (err, decoded) => {
+            if (err) return res.status(401).send({ type: 'error' ,message: 'Unauthorized' })
+            req.user = decoded
+            next()
+        })
+    }
+
+    return {
+        authenticate
+    }
+}
+
+module.exports = JwtHandler;
--- tera/jwt/index.js
+++ tera/jwt/index.js
@@ -1,14 +1,17 @@
 const fp = require("fastify-plugin")
-module.exports = fp(async function (app, options) {
+const JwtHandler = require('./handler.jwt')
+
+const TuosJwt = async function (app, options) {
     app.register(require("fastify-jwt"), {
         secret: options.token
     })
+    app.decorate("jwte", null)
+    
+    const { authenticate } = JwtHandler(app)
+    app.decorate("authenticate", authenticate)
+}
 
-    app.decorate(options.decorate || "authenticate", async function (req, res) {
-        try {
-            await req.jwtVerify()
-        } catch (err) {
-            res.code(401).send({ type: 'error', message: 'Invalid token.', server_message: err.message })
-        }
-    })
+module.exports = fp(TuosJwt, {
+    fastify: '>=3.0.0-alpha.1',
+    name: 'tuos-jwt'
 })
