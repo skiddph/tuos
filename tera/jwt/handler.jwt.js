b9eeff7a3dce8fbdaeba684becbd987f86985b09
eru123 <yeoligoakino@gmail.com>
2021-08-24 15:01:47 +0800

    remove: getToken function

--- tera/jwt/handler.jwt.js
+++ tera/jwt/handler.jwt.js
@@ -1,22 +1,23 @@
 const JwtHandler = (app) => {
   const { Tokens } = app.mongoose.models
 
-  const getToken = (req) => req.headers.authorization.split(' ')[1]
-
   const authenticate = async (req, res, next) => {
-    const token = getToken(req)
-    const userId = app.jwt.decode(token)._id
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      const userId = app.jwt.decode(token)._id
+      req.bearerToken = token
 
-    req.bearerToken = token
+      const record = await Tokens.findOne({ user_id: userId, token })
+      if (!record) return res.status(401).send({ type: 'error', message: 'Unauthorized' })
 
-    const record = await Tokens.findOne({ user_id: userId, token })
-    if (!record) return res.status(401).send({ type: 'error', message: 'Unauthorized' })
-
-    app.jwt.verify(token, null, (err, decoded) => {
-      if (err) return res.status(401).send({ type: 'error', message: 'Unauthorized' })
-      req.user = decoded
-      next()
-    })
+      app.jwt.verify(token, null, (err, decoded) => {
+        if (err) return res.status(401).send({ type: 'error', message: 'Unauthorized' })
+        req.user = decoded
+        next()
+      })
+    } catch (e) {
+      return res.status(401).send({ type: 'error', message: 'Unauthorized' })
+    }
   }
 
   return {
