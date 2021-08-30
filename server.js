9707cefeebeb7b587de10863a559f44b69f8dc6f
eru123 <yeoligoakino@gmail.com>
2021-08-30 16:14:54 +0800

    update: implement bootstrapper

--- server.js
+++ server.js
@@ -1,39 +1,9 @@
 require('dotenv').config()
-require('./tera/api').server({
-  port: process.env.PORT || (() => {
-    console.log('[SYSTEM] Using default PORT')
-    return '8080'
-  })(),
-  host: process.env.HOST || (() => {
-    console.log('[SYSTEM] Using default HOST IP')
-    return '0.0.0.0'
-  })(),
-  token: process.env.JWT_TOKEN || (() => {
-    console.log('[SYSTEM] Using default SECRET_TOKEN')
-    return 'tuos_default_jwt_token'
-  })(),
-  mongo: process.env.MONGO_DIRECT || (() => {
-    console.log('[SYSTEM] Using default DB HOST')
-    return 'mongodb://localhost:27017/tuos'
-  })(),
-  config: require('./tera/api').config,
-  plugins: [
-    require('fastify-cors'),
-    require('./tera/rate-limit'),
-    require('./tera/misc'),
-    require('./tera/mongoose'),
-    require('./tera/mailer'),
-    require('./tera/jwt'),
-    require('./tera/auth').plugin,
-    require('./tera/blog').plugin,
-    require('./tera/my-api').plugin,
-    require('./tera/static')
-  ],
-  db_models: {
-    ...require('./tera/auth').models,
-    ...require('./tera/blog').models,
-    ...require('./tera/my-api').models
-  },
+const bootstrap = require('./bootstrap')
+
+bootstrap({
+  token: process.env.JWT_TOKEN || 'tuos_default_jwt_token',
+  mongo: process.env.MONGO_DIRECT || 'mongodb://localhost:27017/tuos',
   mailer: {
     secure: process.env.MAILER_SECURE === '1',
     host: process.env.MAILER_HOST,
@@ -41,5 +11,9 @@ require('./tera/api').server({
       user: process.env.MAILER_USER,
       pass: process.env.MAILER_PASS
     }
+  },
+  plugins: {
+    excluded: [],
+    primary: ['jwt']
   }
-}).start()
+})
