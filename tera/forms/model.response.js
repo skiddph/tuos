8ca20b31445617beea076e8641a25e18fce063e3
eru123 <yeoligoakino@gmail.com>
2021-09-02 12:14:23 +0800

    update: response schema model

--- tera/forms/model.response.js
+++ tera/forms/model.response.js
@@ -3,38 +3,25 @@ const paginate = require('mongoose-paginate-v2')
 
 module.exports = () => {
   const FormResponse = new mongoose.Schema({
-    alternate_id: { type: String, unique: true, required: true }, // unique id for the quiz
     author_id: { type: String, required: true },
     author_name: { type: String, required: true },
-    use_as: String, // public|class|private
-    origin_id: String, // id of the quiz that this quiz is based on
-    class_id: String, // if associated with a classroom
-    type: String, // form type (quiz|survey|test)
-    title: String,
-    description: String,
     created_at: Number,
     updated_at: Number,
-    has_score: Boolean, // form needs to be scored (applicable on quiz|test)
-    max_attempt: Number, // max number of attempts allowed (applicable on quiz|test) (default: 1)
-    authenticated: Boolean, // authenticated users only (default: false)
-    limit: Number, // max allowed accumulated_time for answering in milliseconds (default: 0)
-    due: Number, // due date for answering the form (is timestamp format) (default: 0)
-    theme: String, // UI Theme (default: default) (theme must be handled by the client)
-    tags: [String], // used for categorizing forms
-    allow_answer_edit: Boolean, // allow editing of answers after submission(default: false)
-    total_points: Number, // total points that can be earn by answering the form (applicable on quiz|test) (default: 0)
-    shuffle_choices: Boolean, // default shuffle choices in every question (default: true)
-    questions: [{
-      id: Number,
-      question: String,
-      type: String, // (single|multiple|manual)
-      points: Number, // points for this specific question (applicable on quiz|test) (default: 1)
-      correct_answer: String, // applicable on single|manual type
-      correct_answers: [String], // applicable on multiple type
-      choices: [String], // applicable on single|multiple type
-      manual_checking: Boolean, // ignore `correct_answer` field. applicable on manual type (applicable on quiz|test form type)
-      shuffle_choices: Boolean, // overwrite shuffle_choices in this question (applicable on multiple type)
-      case_sensitive: Boolean // applicable on manual type
+    form_id: String,
+    form_alternate_id: String,
+    graded: Boolean, // If all questions are graded
+    score: Number, // total scores of all questions
+    accumulated_time: Number, // total time spent on all questions
+    answers: [{
+      id: String, // question number
+      answer: String, // answer (applicable on single|manual type question)
+      answers: [String], // string of answers (applicable on multiple)
+      accumulated_time: Number // time spent in this particular question
+    }],
+    scores: [{
+      id: Number, // question number
+      graded: Boolean, // automatically set to true if the question is not manually graded or else false
+      score: Number // score of the question
     }]
   }, { timestamp: true })
 
