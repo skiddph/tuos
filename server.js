5b306f1bd713d88528ca3f97b27e6c321a14e25b
eru123 <yeoligoakino@gmail.com>
2021-09-07 15:15:13 +0800

    added: tuos mongoose, auth

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
+
 require('dotenv').config()
+// const path = require('path')
 const fastify = require('fastify')({ logger: true })
+// const tera = require('tuos-tera')
 
 const options = {
-  mongoose: {
-    connect: process.env.MONGO_DIRECT || 'mongodb://localhost:27017/tuos'
-  }
+  mongoose: { connect: process.env.MONGO_DIRECT || 'mongodb://localhost:27017/tuos' },
+  jwt: { secret_token: process.env.JWT_TOKEN || 'jwt_default_token' }
 }
 
+// fastify.register(tera, {
+//   dir: path.resolve(__dirname, 'plugins'),
+//   options,
+//   only: ['swagger']
+// })
+
 fastify.register(require('tuos-mongoose'), options)
-fastify.register(require('tuos-tera'), {
-  dir: 'plugins',
-  order: [
-    'jwt',
-    ['auth', 'blog', 'myapi', 'classroom', 'forms']
-  ]
-})
+fastify.register(require('tuos-auth'), options)
+
+// fastify.register(tera, {
+//   dir: path.resolve(__dirname, '../tuos-mongoose/plugins'),
+//   options
+// })
+
+// fastify.register(tera, {
+//   dir: path.resolve(__dirname, '../tuos-auth/plugins'),
+//   options,
+//   order: [
+//     'jwt'
+//   ]
+// })
+
+// fastify.register(tera, {
+//   dir: path.resolve(__dirname, 'plugins'),
+//   options,
+//   only: ['static']
+// })
 
 fastify.listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0')
