20c568fd439825d302d841c797c4c6dd4dfb2c8d
eru123 <yeoligoakino@gmail.com>
2021-09-08 03:58:13 +0800

    update: using submod in dev mode

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
-
 require('dotenv').config()
-// const path = require('path')
+const path = require('path')
 const fastify = require('fastify')({ logger: true })
-// const tera = require('tuos-tera')
 
 const options = {
   mongoose: { connect: process.env.MONGO_DIRECT || 'mongodb://localhost:27017/tuos' },
   jwt: { secret_token: process.env.JWT_TOKEN || 'jwt_default_token' }
 }
 
-// fastify.register(tera, {
-//   dir: path.resolve(__dirname, 'plugins'),
-//   options,
-//   only: ['swagger']
-// })
-
-fastify.register(require('tuos-mongoose'), options)
-fastify.register(require('tuos-auth'), options)
-
-// fastify.register(tera, {
-//   dir: path.resolve(__dirname, '../tuos-mongoose/plugins'),
-//   options
-// })
-
-// fastify.register(tera, {
-//   dir: path.resolve(__dirname, '../tuos-auth/plugins'),
-//   options,
-//   order: [
-//     'jwt'
-//   ]
-// })
-
-// fastify.register(tera, {
-//   dir: path.resolve(__dirname, 'plugins'),
-//   options,
-//   only: ['static']
-// })
+if (process.env.NODE_ENV === 'production') {
+  console.log('[MODE] Production')
+  fastify.register(require('tuos-mongoose'), options)
+  fastify.register(require('tuos-auth'), options)
+  fastify.register(require('tuos-tera'), {
+    dir: path.resolve(__dirname, 'plugins'),
+    options,
+    only: ['static']
+  })
+} else {
+  console.log('[MODE] Development')
+  const tera = require('./core/tera')
+  fastify.register(tera, {
+    dir: path.resolve(__dirname, 'core/mongoose/plugins'),
+    options
+  })
+  fastify.register(tera, {
+    dir: path.resolve(__dirname, 'core/auth/plugins'),
+    options
+  })
+  fastify.register(tera, {
+    dir: path.resolve(__dirname, 'plugins'),
+    options,
+    only: ['static']
+  })
+}
 
 fastify.listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0')
