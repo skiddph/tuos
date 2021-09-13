fbbe16bf1b2fadcef012812ee82a18c2b6ce4056
eru123 <yeoligoakino@gmail.com>
2021-09-14 03:20:29 +0800

    added: mailer

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const path = require('path')
 const fastify = require('fastify')({ logger: true })
 
 const options = {
+  app: {
+    name: 'Tuos',
+    key: 'tuos',
+    version: '1.0.0.dev.1'
+  },
   mongoose: { connect: process.env.MONGO_DIRECT || 'mongodb://localhost:27017/tuos' },
-  jwt: { secret_token: process.env.JWT_TOKEN || 'jwt_default_token' }
+  jwt: { secret_token: process.env.JWT_TOKEN || 'jwt_default_token' },
+  mailer: {
+    // see https://nodemailer.com/
+    transport: {
+      secure: process.env.MAILER_SECURE === '1' || true,
+      host: process.env.MAILER_HOST || 'smtp.gmail.com',
+      port: process.env.MAILER_PORT || 465,
+      auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS
+      }
+    },
+    defaults: {
+      from: process.env.MAILER_FROM
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'production') {
